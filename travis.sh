274174e27b9b143ca4c36cdcf9092d051dc29e3d
Vladislav Yashin <v.yashin.work@gmail.com>
2018-04-03 14:29:20 +0300

    Add libtdjson compilation to travis build

---
+++ travis.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+set -e
+
+export TESTSPACE=`pwd`/testspace
+mkdir $TESTSPACE
+
+cd $TESTSPACE
+
+git clone https://github.com/tdlib/td
+
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . -- - j 4
