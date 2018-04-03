1c22226b4ae674e0971f015ef9fd52dc4e6debb0
Vladislav Yashin <v.yashin.work@gmail.com>
2018-04-03 18:40:24 +0300

    Fix travis cache

--- travis.sh
+++ travis.sh
@@ -2,14 +2,11 @@
 
 set -e
 
-cd home/travis
-
 git init ./td
 cd td
 git remote add origin https://github.com/tdlib/td
 git pull origin master
 
-mkdir -p build
 cd build
 
 cmake -DCMAKE_BUILD_TYPE=Release ..
